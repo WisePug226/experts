2b5d970d722cc644273d94af38d53e705057772a
WisePug226 <87148583+WisePug226@users.noreply.github.com>
2021-11-12 06:59:34 -0500

    Se creó un API  para el frontend

--- backend/index.js
+++ backend/index.js
@@ -30,6 +30,32 @@ app.post('/experts', function(req, res){
     })
 })
 
+// Actualizar totalmente un experto en la DB
+app.put('/experts/:id', function(req, res){
+    const uid = req.params.id;
+    const expert = req.body;
+    dbE.updateExpertTotally(uid, expert, function(status){
+        res.json(status);
+    })
+})
+
+// Actualizar parcialmente un experto en la DB
+app.patch('/experts/:id', function(req, res){
+    const uid = req.params.id;
+    const expert = req.body;
+    dbE.updateExpertPartially(uid, expert, function(status){
+        res.json(status);
+    })
+})
+
+// Borrar un experto de la DB
+app.delete('/experts/:id', function(req, res){
+    const uid = req.params.id;
+    dbE.deleteExpert(uid, function(status){
+        res.json(status);
+    })
+})
+
 app.listen(port, () => {
     console.log("Running on port " + port);
 })
